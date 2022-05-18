1dab2def33b8dffcc3f7b50dcd900641230db892
amirsarfraz <amirsarfarz50@gmail.com>
2022-05-18 21:34:47 +0500

    identify document model

--- src/pages/identify_document_modal/index.page.tsx
+++ src/pages/identify_document_modal/index.page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Button, Modal } from 'react-bootstrap';
 
-import { DivModelButton } from './index.style';
+import { DivModelButton, DivModelButton1, DivModelButton2 } from './index.style';
 
 /**
  *
@@ -21,10 +21,15 @@ function MyVerticallyCenteredModal({ show, onHide }: Props) {
         <p>“Video KYC” Would Like to Access the Camera</p>
         <p>Required for document and facial capture</p>
       </Modal.Body>
+
       <Modal.Footer>
         <DivModelButton>
-          <Button onClick={onHide}>Don’t allow</Button>
-          <Button onClick={onHide}>OK</Button>
+          <DivModelButton1>
+            <Button onClick={onHide}>Don’t allow</Button>
+          </DivModelButton1>
+          <DivModelButton2>
+            <Button onClick={onHide}>OK</Button>
+          </DivModelButton2>
         </DivModelButton>
       </Modal.Footer>
     </Modal>
--- src/pages/identify_document_modal/index.style.ts
+++ src/pages/identify_document_modal/index.style.ts
@@ -3,4 +3,14 @@ import styled from '@emotion/styled';
 export const DivModelButton = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const DivModelButton1 = styled.div`
+  flex-grow: 6;
+  justify-content: flex-start;
+`;
+
+export const DivModelButton2 = styled.div`
+  flex-grow: 6;
 `;
