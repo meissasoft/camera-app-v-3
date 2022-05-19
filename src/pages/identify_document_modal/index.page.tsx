f09d5aabd5551fa15e65b93a051dbd25adfbb30d
amirsarfraz <amirsarfarz50@gmail.com>
2022-05-19 11:24:34 +0500

    identify document model

--- src/pages/identify_document_modal/index.page.tsx
+++ src/pages/identify_document_modal/index.page.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal, Row } from 'react-bootstrap';
 
-import { DivModelButton, DivModelButton1, DivModelButton2 } from './index.style';
+import { BodyModalStyled, IineModel, ParagraphModel, ParagraphTextModel } from './index.style';
 
 /**
  *
@@ -17,21 +17,32 @@ interface Props {
 function MyVerticallyCenteredModal({ show, onHide }: Props) {
   return (
     <Modal show={show} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
-      <Modal.Body>
-        <p>“Video KYC” Would Like to Access the Camera</p>
-        <p>Required for document and facial capture</p>
-      </Modal.Body>
-
-      <Modal.Footer>
-        <DivModelButton>
-          <DivModelButton1>
-            <Button onClick={onHide}>Don’t allow</Button>
-          </DivModelButton1>
-          <DivModelButton2>
-            <Button onClick={onHide}>OK</Button>
-          </DivModelButton2>
-        </DivModelButton>
-      </Modal.Footer>
+      <BodyModalStyled>
+        <Modal.Body>
+          <ParagraphModel>
+            “Video KYC” Would Like to
+            <br />
+            Access the Camera
+          </ParagraphModel>
+          <ParagraphTextModel>
+            Required for document and
+            <br /> facial capture
+          </ParagraphTextModel>
+        </Modal.Body>
+        <IineModel />
+        <Modal.Footer>
+          <Row>
+            <>
+              <div className="col-6">
+                <Button onClick={onHide}>Don’t allow</Button>
+              </div>
+              <div className="col-6">
+                <Button onClick={onHide}>OK</Button>
+              </div>
+            </>
+          </Row>
+        </Modal.Footer>
+      </BodyModalStyled>
     </Modal>
   );
 }
--- src/pages/identify_document_modal/index.style.ts
+++ src/pages/identify_document_modal/index.style.ts
@@ -14,3 +14,68 @@ export const DivModelButton1 = styled.div`
 export const DivModelButton2 = styled.div`
   flex-grow: 6;
 `;
+export const ParagraphModel = styled.p`
+  font-family: 'SF Pro Text';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 17px;
+  line-height: 22px;
+  /* or 129% */
+
+  text-align: center;
+  letter-spacing: -0.408px;
+
+  color: #000000;
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+export const BodyModalStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+
+  position: absolute;
+  width: 270px;
+  height: 160px;
+  left: calc(50% - 270px / 2 + 0.5px);
+  top: calc(50% - 160px / 2);
+  background: rgba(0, 0, 0, 0.8);
+  background: rgba(242, 242, 242, 0.8);
+  backdrop-filter: blur(54.3656px);
+
+  border-radius: 14px;
+`;
+export const ParagraphTextModel = styled.p`
+  font-family: 'SF Pro Text';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 16px;
+
+  text-align: center;
+  letter-spacing: -0.078px;
+
+  color: #000000;
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+`;
+export const IineModel = styled.p`
+  position: absolute;
+  height: 0.5px;
+  left: 0px;
+  right: 0px;
+  top: 0px;
+
+  background: rgba(60, 60, 67, 0.29);
+`;
+export const CenterModel = styled.p`
+  left: calc(50% - 0.5px / 2 - 0.25px);
+  bottom: 0px;
+
+  background: rgba(60, 60, 67, 0.29);
+`;
