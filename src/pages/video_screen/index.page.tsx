b1f91b8fcdf9385d14f514f27795207fabd0160e
amirsarfraz <amirsarfarz50@gmail.com>
2022-05-23 13:03:56 +0500

    fix ui issues

--- src/pages/video_screen/index.page.tsx
+++ src/pages/video_screen/index.page.tsx
@@ -39,7 +39,7 @@ const VideoScreen = () => {
           <Header text="Record video" onClick={onClickHeaderIcon} />
 
           <VerificationCardStyled>
-            <video width="100%" height="420" controls>
+            <video width="100%" height="388" controls>
               <source src={recordedVideo} type="video/mp4" />
             </video>
           </VerificationCardStyled>
